85c82e325496ae974ce5f8a5944775e27a265a3f
Adamo Orsini <adamo.orsini01@gmail.com>
2023-02-18 01:18:42 -0500

    Using yt_dlp instead of youtube dl because of bug

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import datetime
 from pymongo import MongoClient
 from discord.ext import commands
 from discord.utils import get as dget
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 
 # from dotenv import load_dotenv
 # load_dotenv()
