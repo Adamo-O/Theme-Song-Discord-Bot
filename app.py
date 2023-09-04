ac16ac213984f73311124b635f400ccb74f9d03d
Adamo Orsini <adamo.orsini01@gmail.com>
2023-09-04 13:43:43 -0400

    Try user agent header instead of cookies

--- app.py
+++ app.py
@@ -40,8 +40,10 @@ YDL_OPTIONS = {
 	'noplaylist': 'True', 
 	'cookiefile': 'cookies.txt',
 	'skip_download': 'True',
-	'cookiesfrombrowser': ('chrome', ),
-	# 'http_headers': '',
+	# 'cookiesfrombrowser': ('chrome', ),
+	'http_headers': {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+	},
 	# 'postprocessor_args': {
 	# }
 } 
