8a96e694d63c0fc7fae0927858b166d1e9fca84e
Adamo Orsini <adamo.orsini01@gmail.com>
2025-01-12 15:05:28 -0500

    Trying firefox cookies

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ YDL_OPTIONS = {
  	# 'username': 'oauth2',
   # 'password': '',
 	'skip_download': 'True',
-	'cookiesfrombrowser': ('chrome', ),
+	'cookiesfrombrowser': ('firefox', ),
 	# 'http_headers': {
 	# 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 	# },
